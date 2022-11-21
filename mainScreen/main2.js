40dc24b42af87515cf8db14eff8545cfda77c30c
Elis1386 <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2022-11-21 14:36:12 +0200

    add modal cart

--- mainScreen/main2.js
+++ mainScreen/main2.js
@@ -1,20 +1,12 @@
   
-  /* Show modal windows f */
+  /* MODAL FUNCTIONS */
+
   function sendMessageModal() {
     const dialog = document.querySelector("#dialog");
     dialog.showModal();
   }
-  
-  /* Close modal window f */
-  function closeModal()
-  {
-      const closeModal = document.querySelector('#clodeModal')
-      closeModal.closeModal()
-  }
-  
-  
-  let count = document.querySelector('#count')
-  
+
+
   function showCartModal()
   {   
     let amount = count.textContent
@@ -27,23 +19,110 @@
       const cartDialog2 = document.querySelector('.cart_dialog2')
       cartDialog2.showModal()
     }
-    
+  }
+
+
+  function closeModal()
+  {
+      const closeModal = document.querySelector('#clodeModal')
+      closeModal.closeModal()
   }
   
-  /* Add product to the basket */
   
-  // increase baskets count
+
+
+  /* CART FINCTIONS */
+const cartEl = document.querySelector('.cart');
+const cartTotalEl = document.querySelector('.cartTotal');
+const cartTotalValueEl = document.querySelector('.cartTotalValue');
+
+  let cart = {};
+  let count = document.querySelector('#count')
+  // increase cart count
   function increaseCount()
   {
     count.textContent++
   }
-  let basket  = {}
+/**
+ * Метод добавляет продукт в объект cart
+ */
+function addProductToObject(productId) {
+    if (!(productId in cart)) {
+        cart[productId] = 1;
+    } else {
+        cart[productId]++;
+    }
+}
+
+/**
+ * Функция срабатывает когда нужно отрисовать продукт в корзине.
+ * @param {number} productId
+ */
+function renderProductInCart(productId) {
+    let productExist = document.querySelector(`.productCount[data-productId="${productId}"]`);
+    if (productExist) {
+        increaseProductCount(productId);
+        recalculateSumForProduct(productId);
+    } else {
+        renderNewProductInCart(productId);
+    }
+}
+/**
+ * Функция отрисовывает новый товар в корзине.
+ * @param {number} productId
+ */
+function renderNewProductInCart(productId) {
+    let productRow = `
+        <article class="cartRow">
+            <p>${productId.name}</p>
+            <p>$${productId.price}</p>
+            <article>
+                $<span class="productTotalRow" data-productId="${productId}">${productId.price}</span>
+            </article>
+        </article>
+    `;
+    cartTotalEl.insertAdjacentHTML("beforebegin", productRow);
+}
+
+
+/**
+ * Функция пересчитывает стоимость товара умноженное на количество товара
+ * в корзине.
+ * @param {number} productId
+ */
+function recalculateSumForProduct(productId) {
+    const productTotalRowEl = document.querySelector(`.productTotalRow[data-productId="${productId}"]`);
+    let totalPriceForRow = (cart[productId] * products[productId].price).toFixed(2);
+    productTotalRowEl.textContent = totalPriceForRow;
+}
+
+/**
+ * Функция пересчитывает общую стоимость корзины и выводит это значение на страницу.
+ */
+function calculateAndRenderTotalCartSum() {
+    let totalSum = 0;
+    for (let productId in cart) {
+        totalSum += cart[productId] * products[productId].price;
+    }
+    cartTotalValueEl.textContent = totalSum.toFixed(2);
+}
+
+
+/**
+ * Эта функция срабатывает когда добавляют новый товар в корзину.
+ * @param {number} productId
+ */
+function addProductIntoCart(productId) {
+    increaseCount();
+    addProductToObject(productId);
+    renderProductInCart(productId);
+    calculateAndRenderTotalCartSum();
+}
+
   
-  // total amount 
-  function totalSum()
-  {
+
   
-  }
+  
   
 
 
--- mainScreen/productsCards.js
+++ mainScreen/productsCards.js
@@ -10,7 +10,7 @@ function getProductMarkup(product) {
     <a href="../onePhotoPage/index.html?id=${product.id}" class="cart">
       <img src="${pathToProductsImages}/${product.image}" class="item__img" alt="${product.name}" />
     </a>
-    <figcaption class="add_to_cart" data-productID="${product.id}">
+    <figcaption class="add_to_cart" data-productId="${product.id}">
       <a class="add" href="#" onclick="increaseCount(); return false"><i class="fa-solid fa-cart-shopping pop-up-cart"></i>Add to
         cart</a>
     </figcaption>
@@ -36,7 +36,6 @@ function addEventForAddToCart() {
     const addToCart = document.querySelectorAll('figcaption[data-productID]');
     addToCart.forEach(function (btn) {
         btn.addEventListener('click', addedProductHandler);
-        console.log('click');
     })
 }
 
@@ -44,18 +43,13 @@ function addEventForAddToCart() {
  * Функция-обработчик события клика по кнопке "Add to cart".
  */
 function addedProductHandler(event) {
-    const productID = event.currentTarget.getAttribute('data-productID');
-    console.log(productID)
-    addProduckIntoBasket()
-}
-/**
- * Функция-добавляет продукты в корзину.
- */
-
-function addProduckIntoBasket(productID)
-{
-  
+    const productId = event.currentTarget.getAttribute('data-productId');
+    console.log(productId)
+    addProductIntoCart()
 }
 
 insertProductsIntoPage(products, items);
 addEventForAddToCart();
+
+
+
